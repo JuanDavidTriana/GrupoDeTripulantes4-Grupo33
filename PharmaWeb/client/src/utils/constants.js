d24f578daa4b5dfee53674f6e29d646de84cf808
chrisVan-G <chrisvaec@gmail.com>
2021-10-16 08:28:16 -0500

    últmos cambios

--- PharmaWeb/client/src/utils/constants.js
+++ PharmaWeb/client/src/utils/constants.js
@@ -1,3 +1,4 @@
-export const API_URL = "http://localhost:1337"
+//export const API_URL = "http://localhost:1337"
+export const API_URL = "https://app-pharmaweb.herokuapp.com"
 export const TOKEN = "token";
 export const PRODUCTS = "products" 
